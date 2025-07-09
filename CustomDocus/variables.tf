030575ee2c1d55d14b2424741f5e8221d705ed6d
Lewis Webb <lewiswebb@mac.lan>
2025-07-09 03:07:17 +0100

    vars updated

--- CustomDocus/variables.tf
+++ CustomDocus/variables.tf
@@ -1,8 +1,9 @@
 variable "admin_username" {
   description = "The administrator username for the virtual machine."
+  type        = string
 }
 
 variable "admin_password" {
   description = "The admin password for the VM"
-  sensitive   = true
+  type        = string
 }
