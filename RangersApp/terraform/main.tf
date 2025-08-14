75240196f6076d3faa4cd2bc3795131b2ff96790
Lewis Webb <webblcw23@hotmail.com>
2025-08-14 19:23:57 +0100

    vars into provider

--- RangersApp/terraform/main.tf
+++ RangersApp/terraform/main.tf
@@ -1,11 +1,16 @@
 # RangersApp Terraform Configuration
 
 # Provider Configuration
+
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   subscription_id = var.subscription_id
-    tenant_id       = var.tenant_id
+  tenant_id       = var.tenant_id
 }
+
 
 # Resource Group
 resource "azurerm_resource_group" "rg" {
