c05a153aa494b1de46c17f2a5f25aaa5f8c96897
Lewis Webb <webblcw23@hotmail.com>
2025-08-14 18:36:40 +0100

    ado to azure vars

--- RangersApp/terraform/variables.tf
+++ RangersApp/terraform/variables.tf
@@ -30,3 +30,26 @@ variable "docker_image_tag" {
   type        = string
   default     = "latest"
 }
+
+#Pipeline connection variables from Azure DevOps to Azure
+
+variable "client_id" {
+  description = "Azure service principal client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure service principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
