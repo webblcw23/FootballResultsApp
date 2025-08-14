27fedea12c45d83ececec4375e0476441580b7f0
Lewis Webb <webblcw23@hotmail.com>
2025-08-14 19:34:36 +0100

    remove azure client vars

--- RangersApp/terraform/main.tf
+++ RangersApp/terraform/main.tf
@@ -4,11 +4,6 @@
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 
