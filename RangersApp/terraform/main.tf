3acdd7d1c1a27ff7f45abd1105f6dedb1bd0d285
Lewis Webb <lewiswebb@mac.lan>
2025-08-22 16:27:13 +0100

    ready to test with pipelien and infra set up

--- RangersApp/terraform/main.tf
+++ RangersApp/terraform/main.tf
@@ -1,51 +1,37 @@
-# RangersApp Terraform Configuration
-
-# Provider Configuration
-
+# Connect to Azure
 provider "azurerm" {
+  subscription_id = "91c0fe80-4528-4bf2-9796-5d0f2a250518"
   features {}
 }
 
-
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# Creation of Azure resources for Rangers App
+resource "azurerm_resource_group" "rangers" {
+  name     = "rg-rangers-app"
+  location = "UK South"
 }
 
-# Azure container registry
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
+# Creation of Azure resources for Rangers App Service Plan and Web App
+resource "azurerm_service_plan" "rangers_plan" {
+  name                = "asp-rangers"
+  location            = azurerm_resource_group.rangers.location
+  resource_group_name = azurerm_resource_group.rangers.name
+  sku_name            = "B1"
+  os_type             = "Windows"
 }
 
-# Azure Service Plan
-resource "azurerm_service_plan" "asp" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-    sku_name = "B1" 
-    os_type = "Windows"
-  }
-
-# Azure Windows Web App
-resource "azurerm_windows_web_app" "app" {
-  name                = var.web_app_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id = azurerm_service_plan.asp.id
-
-  site_config {
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/rangersapp:latest"
-  }
+# Creation of Azure resources for Rangers App Service
+resource "azurerm_windows_web_app" "rangers_web" {
+  name                = "rangers-app"
+  location            = azurerm_resource_group.rangers.location
+  resource_group_name = azurerm_resource_group.rangers.name
+  service_plan_id     = azurerm_service_plan.rangers_plan.id
 
   app_settings = {
-    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
-    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
-    "WEBSITES_PORT"                   = "80"
+    "WEBSITE_NODE_DEFAULT_VERSION" = "14.17"
+    "RANGERS_API_URL"              = "https://api.rangersapp.com"
+  }
+
+  site_config {
+    always_on = true
   }
 }
