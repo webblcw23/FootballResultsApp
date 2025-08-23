e8741e5a9698f121260543bfb97681c099863883
Lewis Webb <webblcw23@hotmail.com>
2025-08-23 02:42:09 +0100

    data block for first run

--- RangersApp/terraform/main.tf
+++ RangersApp/terraform/main.tf
@@ -11,13 +11,13 @@ provider "azurerm" {
 }
 
 # Resource Group
-resource "azurerm_resource_group" "rg" {
+data "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
 # Azure container registry
-resource "azurerm_container_registry" "acr" {
+data "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
