d17a3dc3bbda154b909f3493f8e510f637d23b47
Lewis Webb <webblcw23@hotmail.com>
2025-08-23 04:32:50 +0100

    updated main.tf

--- RangersApp/terraform/main.tf
+++ RangersApp/terraform/main.tf
@@ -11,25 +11,25 @@ provider "azurerm" {
 }
 
 # Resource Group
-#resource "azurerm_resource_group" "rg" {
-#  name     = var.resource_group_name
-#   location = var.location
-#}
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+   location = var.location
+}
 
 # Azure container registry
-#resource "azurerm_container_registry" "acr" {
-#  name                = var.acr_name
-#  resource_group_name = azurerm_resource_group.rg.name
-#   location            = var.location
-#  sku                 = "Basic"
-#  admin_enabled       = true
-#}
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+   location            = var.location
+  sku                 = "Basic"
+   admin_enabled       = true
+}
 
 # Azure Service Plan
 resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
     sku_name = "B1" 
     os_type = "Linux"
   }
@@ -37,12 +37,16 @@ resource "azurerm_service_plan" "asp" {
 # Azure Linux Web App
 resource "azurerm_linux_web_app" "app" {
   name                = "rangersapp"
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   service_plan_id     = azurerm_service_plan.asp.id
 
-  container_settings {
-    image_name        = "${var.acr_name}/rangersapp:latest"
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/rangersapp:latest"
+  }
+
+  app_settings = {
+    "WEBSITES_PORT" = "80"
   }
 
   identity {
