a6e814033dbd277e957bcee3bde7467b890a5f73
Lewis Webb <webblcw23@hotmail.com>
2025-08-27 05:18:44 +0100

    back to latest

--- RangersApp/terraform/main.tf
+++ RangersApp/terraform/main.tf
@@ -73,7 +73,7 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {
     application_stack {
-      docker_image_name        = "rangersapp:v1"  # Just the repo + tag
+      docker_image_name        = "rangersapp:latest"  # Just the repo + tag
       docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
       docker_registry_username = azurerm_container_registry.acr.admin_username
       docker_registry_password = azurerm_container_registry.acr.admin_password
