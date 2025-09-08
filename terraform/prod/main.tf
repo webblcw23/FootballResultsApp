f039501568a6ca8dcba46021f4a671ea74ab754f
Lewis Webb <webblcw23@hotmail.com>
2025-09-08 05:43:44 +0100

    reinstate provider info

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rangers_rg_prod" {
